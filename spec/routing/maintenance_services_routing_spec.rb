3dcfd94d3b3261b901cb02627d18102b166e78e5
Serlle Rosales <serlle.rosales96@gmail.com>
2025-09-19 14:31:55 -0600

    spec(routing): expect nested maintenance_service routes under vehicles

--- spec/routing/maintenance_services_routing_spec.rb
+++ spec/routing/maintenance_services_routing_spec.rb
@@ -3,36 +3,35 @@ require "rails_helper"
 RSpec.describe MaintenanceServicesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/maintenance_services").to route_to("maintenance_services#index")
+      expect(get: "/vehicles/1/maintenance_services").to route_to("maintenance_services#index", vehicle_id: "1")
     end
 
     it "routes to #new" do
-      expect(get: "/maintenance_services/new").to route_to("maintenance_services#new")
+      expect(get: "/vehicles/1/maintenance_services/new").to route_to("maintenance_services#new", vehicle_id: "1")
     end
 
     it "routes to #show" do
-      expect(get: "/maintenance_services/1").to route_to("maintenance_services#show", id: "1")
+      expect(get: "/vehicles/1/maintenance_services/1").to route_to("maintenance_services#show", vehicle_id: "1", id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/maintenance_services/1/edit").to route_to("maintenance_services#edit", id: "1")
+      expect(get: "/vehicles/1/maintenance_services/1/edit").to route_to("maintenance_services#edit", vehicle_id: "1", id: "1")
     end
 
-
     it "routes to #create" do
-      expect(post: "/maintenance_services").to route_to("maintenance_services#create")
+      expect(post: "/vehicles/1/maintenance_services").to route_to("maintenance_services#create", vehicle_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/maintenance_services/1").to route_to("maintenance_services#update", id: "1")
+      expect(put: "/vehicles/1/maintenance_services/1").to route_to("maintenance_services#update", vehicle_id: "1", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/maintenance_services/1").to route_to("maintenance_services#update", id: "1")
+      expect(patch: "/vehicles/1/maintenance_services/1").to route_to("maintenance_services#update", vehicle_id: "1", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/maintenance_services/1").to route_to("maintenance_services#destroy", id: "1")
+      expect(delete: "/vehicles/1/maintenance_services/1").to route_to("maintenance_services#destroy", vehicle_id: "1", id: "1")
     end
   end
 end
