c1b63d331d0e5fb12403a069abed05435b933a68
Serlle Rosales <serlle.rosales96@gmail.com>
2025-09-22 12:56:39 -0600

    chore: change the serializer as official AMS way on the maintenance service serializer

--- app/serializers/maintenance_service_serializer.rb
+++ app/serializers/maintenance_service_serializer.rb
@@ -1,13 +1,18 @@
 class MaintenanceServiceSerializer < ActiveModel::Serializer
   attributes :id, :description, :status, :date, :cost_cents, :priority, :completed_at, :vehicle_id, :vehicle
 
-  # include vehicle id for easy lookups and a compact vehicle object
-  def vehicle_id
-    object.vehicle_id
-  end
+  # Official AMS way
+  belongs_to :vehicle
 
-  def vehicle
-    return unless object.vehicle
-    VehicleSerializer.new(object.vehicle, scope: scope, root: false)
-  end
+  # Explicitly way
+
+  # # include vehicle id for easy lookups and a compact vehicle object
+  # def vehicle_id
+  #   object.vehicle_id
+  # end
+
+  # def vehicle
+  #   return unless object.vehicle
+  #   VehicleSerializer.new(object.vehicle, scope: scope, root: false)
+  # end
 end
