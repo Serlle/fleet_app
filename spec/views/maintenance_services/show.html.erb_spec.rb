6a8be3dd2ed83b7994f3b602fff741c3029a4205
Serlle Rosales <serlle.rosales96@gmail.com>
2025-09-22 19:50:11 -0600

    fix(testing): change test on index and show rspec

--- spec/views/maintenance_services/show.html.erb_spec.rb
+++ spec/views/maintenance_services/show.html.erb_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "maintenance_services/show", type: :view do
-  before(:each) do
-  vehicle = FactoryBot.create(:vehicle, vin: "VIN777", plate: "PL777", brand: "B", model: "M", year: 2001, status: :active)
-  assign(:maintenance_service, FactoryBot.create(:maintenance_service, vehicle: vehicle, description: "MyText", status: :completed, cost_cents: 300, priority: :high, date: Date.today))
+  before do
+    vehicle = FactoryBot.create(:vehicle, vin: "VIN999", plate: "PL999", brand: "B", model: "M", year: 2000, status: :active)
+    assign(:maintenance_service,
+      FactoryBot.create(:maintenance_service, :completed, vehicle: vehicle,
+             description: "MyText", cost_cents: 300, priority: :high, date: Date.today)
+    )
   end
-
   it "renders attributes in <p>" do
     render
   expect(rendered).to match(/MyText/)
